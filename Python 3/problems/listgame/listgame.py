d142c6bea02fa1fd0dda5bb86fa92f30426b24b6
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2022-07-06 20:08:47 +0200

    Testing a cleaner method
    Status
    Wrong Answer
    
    CPU
    0.05 s
    
    Test cases
    3/20

--- Python 3/problems/listgame/listgame.py
+++ Python 3/problems/listgame/listgame.py
@@ -2,50 +2,26 @@
 # Link: https://open.kattis.com/problems/listgame
 
 import sys
-from math import gcd as gcd
-from math import sqrt as sqrt
 
-# # Kattis / Machine input
-# for i in sys.stdin:
-#     input = int(i)
+# Kattis / Machine input
+for i in sys.stdin:
+    input = int(i)
 
-# Manual input
-input = 65536
-
-# Functions
-def check_coPrime(a,b):
-    isCoprime = (gcd(a,b)==1)
-    
-    return isCoprime
+# # Manual input
+# input = 65536
 
 ### Main
-cop_factors = []
-for i in range(2,int(sqrt(input))):
-    if (input % i == 0):
-        if len(cop_factors) == 0:
-            cop_factors.append(i)
-        else:
-            for j in cop_factors:
-                if check_coPrime(i, j):
-                    cop_factors.append(i)
-
-# print(cop_factors)
-
 ans = input
-k = 1
+div = 2
+k = 0
 
-while ans > 1:
-    for div in cop_factors:
-        while True:
-            ans = ans / div
-            if int(ans) == ans and ans != 1:
-                k += 1
-                continue
-            elif (int(ans) != ans):
-                ans = ans * div
-                break
-            else:
-                break
+while ans != 1:
+    ans /= div
+    if int(ans) == ans:
+        k += 1
+    else:
+        ans = int(ans*div)
+        div += 1
 
 print(k)
 # print("Done!")

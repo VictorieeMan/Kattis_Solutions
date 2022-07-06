0047ff70d5f43d7c12218d8752e9504d238ee00a
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2022-07-06 16:37:29 +0200

    Update betting.py

--- Python 3/problems/betting/betting.py
+++ Python 3/problems/betting/betting.py
@@ -1,7 +1,12 @@
 # Created: 2022-07-06
 # https://open.kattis.com/problems/betting
 
-input = int(input("Percentage of switchpoints on option 1: "))
+import sys
+
+for i in sys.stdin:
+    input = int(i)
+
+# input = int(input("Percentage of switchpoints on option 1: "))
 
 op1 = 100/input
 op2 = 100/(100-input)

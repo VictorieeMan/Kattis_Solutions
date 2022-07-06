dc6d89dc86ba85ac248812c42800f447f8657ea9
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2022-07-06 21:38:28 +0200

    Create helpaphd.py

---
+++ Python 3/problems/helpaphd/helpaphd.py
@@ -0,0 +1,15 @@
+# Created: 2022-07-06
+# Link: https://open.kattis.com/problems/helpaphd
+
+#Recommended from the guide: https://github.com/VictorieeMan/kattis-guide/blob/master/input.md
+
+import sys
+
+# # Kattis / Machine input
+# for i in sys.stdin:
+#     input = int(i)
+
+# Manual input
+input = "4\n\2+2\1+2\p=NP\0+0"
+
+print("Done!")

87fd1622dd6f72296460112fceb2666139b7728b
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2022-07-06 21:53:00 +0200

    Status Accepted

--- Python 3/problems/helpaphd/helpaphd.py
+++ Python 3/problems/helpaphd/helpaphd.py
@@ -5,11 +5,19 @@
 
 import sys
 
-# # Kattis / Machine input
-# for i in sys.stdin:
-#     input = int(i)
+# Kattis / Machine input
+input = sys.stdin.read()
 
 # Manual input
-input = "4\n\2+2\1+2\p=NP\0+0"
+# input = "4\n\2+2\1+2\p=NP\0+0"
+
+string = input.split("\n")[1:-1]
+
+for i in string:
+    if i == "P=NP":
+        print("skipped")
+    else:
+        add = i.split("+")
+        print(int(add[0])+int(add[1]))
 
 print("Done!")

0546879627171161d59dc35c3f84e75fcb34ce8d
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2022-07-06 21:09:56 +0200

    Create oddecho.py

---
+++ Python 3/problems/oddecho/oddecho.py
@@ -0,0 +1,11 @@
+# Created: 2022-07-06
+# Link: https://open.kattis.com/problems/oddecho
+
+import sys
+
+# # Kattis / Machine input
+# for i in sys.stdin:
+#     input = int(i)
+
+# Manual input
+input = "5\nhello\ni\nam\nan\necho"

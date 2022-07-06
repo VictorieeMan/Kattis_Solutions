d45457f8e47e5fa46cdc799eddb86d18e7b1a818
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2022-07-06 16:40:20 +0200

    Create listgame.py

---
+++ Python 3/problems/listgame/listgame.py
@@ -0,0 +1,11 @@
+# Created: 2022-07-06
+# Link: https://open.kattis.com/problems/listgame
+
+import sys
+
+# # Kattis / Machine input
+# for i in sys.stdin:
+#     input = int(i)
+
+# Manual input
+input = 6536

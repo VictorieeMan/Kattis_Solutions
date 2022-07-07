90419e10293397b841084b8930494c1ac9489ceb
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2022-07-07 12:13:33 +0200

    Update sylvester.cpp
    Added the template code

--- C++/problems/sylvester/sylvester.cpp
+++ C++/problems/sylvester/sylvester.cpp
@@ -1,5 +1,17 @@
+// Created: 2022-07-07
+// Link: https://open.kattis.com/problems/sylvester
+
+// Kattis allows all standard libraries included in C++
 #include <iostream>
+#include <algorithm>
 
 int main() {
-    std::cout << "Hello World!";
+    // Standard input, alter as needed
+    long long a, b;
+    std::cin >> a >> b;
+
+    // Main code
+    // std::cout << "Hello World!";
+
+    return 0;
 }

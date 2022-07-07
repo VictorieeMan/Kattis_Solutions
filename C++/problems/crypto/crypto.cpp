264693c20bfb741e00736d156baf72d06f4e5ac3
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2022-07-07 15:41:27 +0200

    Wrote base_conversion function

--- C++/problems/crypto/crypto.cpp
+++ C++/problems/crypto/crypto.cpp
@@ -1,5 +1,5 @@
-// Created: 2022-
-// Link: 
+// Created: 2022-07-07
+// Link: https://open.kattis.com/problems/crypto
 
 // Kattis allows all standard libraries included in C++
 #include <algorithm>
@@ -7,10 +7,34 @@
 #include <string>
 #include <vector>
 
+std::vector<int> base_conversion(int p, int b){
+    std::vector<int> in_new_base;
+    int r = (p % b);
+    int q = (p-r)/b;
+
+    while(q != 0){
+        in_new_base.push_back(r);
+        p = q;
+        r = (p % b);
+        q = (p-r)/b;
+    }
+    
+    in_new_base.push_back(r);
+
+    return in_new_base;
+}
+
 int main() {
+    std::cout << "Hello World! 1";
     // Standard input, alter as needed
-    long long a, b;
-    std::cin >> a >> b;
+    long long a;
+    std::cin >> a;
+    std::cout << "Hello World! 2";
+
+    std::vector<int> in_new_base = base_conversion(a,2);
+    for(int i = 0; i < in_new_base.size();i++){
+        std::cout << in_new_base[i];
+    }
 
     // std::cout << "Hello World!";
 

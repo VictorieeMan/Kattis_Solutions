60f4420df0417447bff18e26c5ef99063d88c549
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2022-07-07 17:14:55 +0200

    Create twostones.cpp

---
+++ C++/problems/twostones/twostones.cpp
@@ -0,0 +1,18 @@
+// Created: 2022-07-07
+// Link: https://open.kattis.com/problems/twostones
+
+// Kattis allows all standard libraries included in C++
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+    // Standard input, alter as needed
+    long long a, b;
+    std::cin >> a >> b;
+
+    // std::cout << "Hello World!";
+
+    return 0;
+}

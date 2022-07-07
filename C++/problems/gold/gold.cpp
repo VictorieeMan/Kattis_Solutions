1c53343697cf56eb1f43ad3d3b6748098abd835d
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2022-07-07 12:26:03 +0200

    Create gold.cpp

---
+++ C++/problems/gold/gold.cpp
@@ -0,0 +1,17 @@
+// Created: 2022-
+// Link: 
+
+// Kattis allows all standard libraries included in C++
+#include <iostream>
+#include <algorithm>
+
+int main() {
+    // Standard input, alter as needed
+    long long a, b;
+    std::cin >> a >> b;
+
+    // Main code
+    // std::cout << "Hello World!";
+
+    return 0;
+}

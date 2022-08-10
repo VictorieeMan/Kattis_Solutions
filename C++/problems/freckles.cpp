0021489e02bf1e8ad06048e7c51918f30ba111c9
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2022-08-10 15:38:13 +0200

    Update freckles.cpp
    Started writing a shortest path algorithm, but realized this is a "Minimum spanning tree" problem.

--- C++/problems/freckles.cpp
+++ C++/problems/freckles.cpp
@@ -6,6 +6,34 @@
 #include <iostream>
 // #include <string>
 #include <vector>
+#include <cmath>
+
+float distance(std::pair<float,float> p1, std::pair<float,float> p2){
+    return sqrt(pow(p1.first-p2.first,2)+pow(p1.second-p2.second,2));
+}
+
+float shortest_path_alg(std::vector<std::pair<float,float>> points){
+    // Using the FLoyd-Warshall algorithm to find the shortest path between all points
+    // https://en.wikipedia.org/wiki/Floyd%E2%80%93Warshall_algorithm
+    float shortest_path = 0;
+    int n = points.size();
+    std::vector<std::vector<float>> dist(n, std::vector<float>(n, 0));
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            dist[i][j] = distance(points[i], points[j]);
+        }
+    }
+
+    return shortest_path;
+}
+
+/*
+    float shortest_path = 0;
+    for(int i = 0; i < points.size()-1; i++){
+        shortest_path += distance(points[i],points[i+1]);
+    }
+    return shortest_path;
+*/
 
 int main() {
     // N is the number of test cases
@@ -24,7 +52,8 @@ int main() {
             std::cin >> x >> y;
             freckles.push_back(std::make_pair(x, y));
         }
-
+        float shrt_path = shortest_path_alg(freckles);
+        std::cout << shrt_path << std::endl;
     }
 
     return 0;

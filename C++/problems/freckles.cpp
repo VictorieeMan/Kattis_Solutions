0aeeaf535f8df1ec8fe9a7a962839ec93238a2f2
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2022-08-10 15:13:48 +0200

    Added distance function

--- C++/problems/freckles.cpp
+++ C++/problems/freckles.cpp
@@ -2,17 +2,30 @@
 // Link: https://open.kattis.com/problems/freckles
 
 // Kattis allows all standard libraries included in C++
-#include <algorithm>
+// #include <algorithm>
 #include <iostream>
-#include <string>
+// #include <string>
 #include <vector>
 
 int main() {
-    // Standard input, alter as needed
-    long long a, b;
-    std::cin >> a >> b;
+    // N is the number of test cases
+    // n is the number of freckles
+    // x, y are the coordinates of the freckles
 
-    // std::cout << "Hello World!";
+    //input & process loop
+    int N;
+    std::cin >> N;
+    for(int i = 0; i < N; i++){
+        int n;
+        std::cin >> n;
+        std::vector<std::pair<float, float>> freckles;
+        for(int j = 0; j < n; j++){
+            float x, y;
+            std::cin >> x >> y;
+            freckles.push_back(std::make_pair(x, y));
+        }
+
+    }
 
     return 0;
 }

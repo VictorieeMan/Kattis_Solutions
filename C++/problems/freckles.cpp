15f2960668bc325f1d6b8871c5f887e9df60bb2c
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2022-08-10 12:14:11 +0200

    Added new problem freckles

---
+++ C++/problems/freckles.cpp
@@ -0,0 +1,18 @@
+// Created: 2022-08-10
+// Link: https://open.kattis.com/problems/freckles
+
+// Kattis allows all standard libraries included in C++
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+    // Standard input, alter as needed
+    long long a, b;
+    std::cin >> a >> b;
+
+    // std::cout << "Hello World!";
+
+    return 0;
+}

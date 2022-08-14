61c1462a650e5119ecace1dab27f445757e38cca
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2022-08-14 21:15:30 +0200

    Inser edges ascendingly to edges list

--- C++/problems/freckles.cpp
+++ C++/problems/freckles.cpp
@@ -49,9 +49,114 @@ double shortest_path_alg(std::vector<std::pair<double,double>> points){
     return min_dist;
 }
 
+// Insert new edge in ascending order of distances
+std::vector<std::tuple<int, int, double>> insert_new_edge_ascOrder(
+    std::vector<std::tuple<int, int, double>> edges,
+    std::tuple<int, int, double> new_edge
+    ){
+        int n = edges.size();
+        for(int i = 0; i<n; i++){
+            if(std::get<2>(new_edge) < std::get<2>(edges[i])){
+                edges.insert(edges.begin()+i, new_edge);
+                return edges;
+            }
+        }
+        return edges;
+}
+
+// Check if edge is calculated
+bool edge_is_calculated(int a, int b, std::vector<std::tuple<int, int, double>> edges){
+    for(int i=0; i<edges.size();i++){
+        if(std::get<0>(edges[i]) == a && std::get<1>(edges[i]) == b){
+            return true;
+        }
+        if(std::get<0>(edges[i]) == b && std::get<1>(edges[i]) == a){
+            return true;
+        }
+    }
+    return false;
+}
+
+// Calculating the spokes of a node, to all free_nodes, and updates the list of edges.
+void calculate_spokes(
+    std::vector<std::tuple<int, int, double>>& edges, 
+    std::vector<std::tuple<int, double, double>> free_nodes, 
+    std::tuple<int, double, double> node
+    ){
+    int n = free_nodes.size();
+    for(int i=0; i<n;i++){
+        std::tuple<int, int, double> new_edge;
+        if(!edge_is_calculated(std::get<0>(node), std::get<0>(free_nodes[i]), edges)){
+            new_edge = std::make_tuple(std::get<0>(node), std::get<0>(free_nodes[i]), node_distance(node, free_nodes[i]));
+            edges = insert_new_edge_ascOrder(edges, new_edge);
+        }
+    }
+}
+
+// Get the edge, from index pair
+std::tuple<int, int, double> get_edge(int a, int b, std::vector<std::tuple<int, int, double>> edges){
+    for(int i=0; i<edges.size();i++){
+        if(std::get<0>(edges[i]) == a && std::get<1>(edges[i]) == b){
+            return edges[i];
+        }
+        if(std::get<0>(edges[i]) == b && std::get<1>(edges[i]) == a){
+            return edges[i];
+        }
+    }
+    return std::make_tuple(0,0,0);
+}
+
+// Get the edge length, from index pair
+double get_edge_length(int a, int b, std::vector<std::tuple<int, int, double>> edges){
+    return std::get<2>(get_edge(a, b, edges));
+}
+
+// Find shortest edge from visited to a free node
+std::tuple<int, int, double> find_shortest_new_branch(
+    std::vector<std::tuple<int, int, double>> edges, 
+    std::vector<std::tuple<int, double, double>> nodes,
+    std::vector<std::tuple<int, double, double>> visited, 
+    std::vector<std::tuple<int, double, double>> free_nodes
+    ){
+        std::tuple<int, int, double> new_visit = free_nodes.back();
+        double edge_length = get_edge_length(std::get<0>(new_visit), std::get<0>(visited.back()), edges);
+        int n = free_nodes.size();
+        for(int i=0; i<n;i++){
+            
+        }
+        return new_visit;
+}
+
+
 // Prim algorithm to find the shortest path
-double minimal_spanning_tree_prim(std::vector<std::tuple<int, double, double>> points){
+double minimal_spanning_tree_prim(std::vector<std::tuple<int, double, double>> nodes){
+    std::vector<std::tuple<int, int, double>> edges;
+    // (node_i_idx, node_j_idx, distance/weight)
+
+    std::vector<std::tuple<int, double, double>> free_nodes = nodes;
+    std::vector<std::tuple<int, double, double>> visited; // Also knows as the tree
+    // (node_idx, node_i_x, node_i_y)
+
+    // Initialize the visited nodes with the first node
+    visited.push_back(free_nodes.back());
+    free_nodes.pop_back();
     double mst_len = 0;
+    int i = 0;
+
+    while(free_nodes.size() > 0){
+        std::tuple<int, double, double> node_i = visited.back();
+
+        // Calculte the node_i spokes, and add them to list of edges
+        calculate_spokes(edges, free_nodes, node_i);
+        // Find the shortest edge from the visited nodes to a free node
+        find_shortest_new_branch(edges, nodes, visited, free_nodes);
+
+        // Update for next iteration
+        visited.push_back(free_nodes.back());
+        free_nodes.pop_back();
+        i++;
+    }
+
     return mst_len;
 }
 

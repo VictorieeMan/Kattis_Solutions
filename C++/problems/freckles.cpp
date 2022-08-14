12757f8afb4d6755ff264e32b6b158f7f5fab4f5
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2022-08-14 18:42:44 +0200

    Starting from checkpoint
    The input is good! And the help functions are good, let's remake the mst algorithm from scratch.

--- C++/problems/freckles.cpp
+++ C++/problems/freckles.cpp
@@ -26,85 +26,33 @@ double node_distance(std::tuple<int, double, double> n1, std::tuple<int, double,
     return distance(p1, p2);
 }
 
-// double shortest_path_alg(std::vector<std::pair<double,double>> points){
-//     // Greedy algorithm to find the shortest path
-//     std::vector<std::pair<double,double>> visited;
-//     visited.push_back(points.back());
-//     points.pop_back();
+double shortest_path_alg(std::vector<std::pair<double,double>> points){
+    // Greedy algorithm to find the shortest path
+    std::vector<std::pair<double,double>> visited;
+    visited.push_back(points.back());
+    points.pop_back();
 
-//     double min_dist = 0;
-//     int n = points.size();
+    double min_dist = 0;
+    int n = points.size();
 
-//     for(int i=0; i<n;i++){
-//         std::vector<double> distances(points.size());
-//         for(int j=0; j<points.size();j++){
-//             distances[j] = distance(visited.back(),points[j]);
-//         }
-//         int min_index = std::min_element(distances.begin(),distances.end()) - distances.begin();
-//         min_dist += distances[min_index];
-//         visited.push_back(points[min_index]);
-//         points.erase(points.begin()+min_index);
-//     }
-
-//     return min_dist;
-// }
-
-// Find the shortest spoke
-std::tuple<int,int,double> shortest_spoke(std::vector<std::vector<std::tuple<int,int,double>>> spokes){
-    int n = spokes.size();
-    std::tuple<int,int,double> min_spoke = spokes[0][0];
     for(int i=0; i<n;i++){
-        for(int j=0; j<spokes[i].size();j++){
-            if(std::get<2>(spokes[i][j]) < std::get<2>(min_spoke)){
-                min_spoke = spokes[i][j];
-            }
+        std::vector<double> distances(points.size());
+        for(int j=0; j<points.size();j++){
+            distances[j] = distance(visited.back(),points[j]);
         }
+        int min_index = std::min_element(distances.begin(),distances.end()) - distances.begin();
+        min_dist += distances[min_index];
+        visited.push_back(points[min_index]);
+        points.erase(points.begin()+min_index);
     }
-    return min_spoke;
+
+    return min_dist;
 }
 
 // Prim algorithm to find the shortest path
-double minimal_spanning_tree_prim(std::vector<std::tuple<int, double, double>> points){ 
-    std::vector<std::tuple<int, double, double>> free_points = points;
-    // Unattached points are free
-
-    std::tuple<int, double, double> node_i = free_points.back();
-    free_points.pop_back();
-
-    std::vector<std::vector<std::tuple<int,int,double>>> spokes(free_points.size());
-    // Data type: std::tuple<int,int,double>
-    // [index of the node_i, the index of the free_point_j, and their distance]
-
-    double mst_length = 0;
-    int n = free_points.size();
-
-    for(int i=0; i<n;i++){
-        std::tuple<int,int,double> distance;
-        int idx_node_i = std::get<0>(node_i);
-        for(int j=0;j<free_points.size();j++){
-            int idx_freeP_j = std::get<0>(free_points[j]);
-            distance = std::make_tuple(idx_node_i,idx_freeP_j,node_distance(node_i,free_points[j]));
-            spokes[i].push_back(distance);
-        }
-
-        // Find shortest spoke and add to the mst_length
-        std::tuple<int,int,double> min_spoke = shortest_spoke(spokes);
-        mst_length += std::get<2>(min_spoke);
-
-        // Remove the spoke from the free_points
-        int idx_freeP_j = std::get<1>(min_spoke);
-        free_points.erase(free_points.begin()+idx_freeP_j);
-        
-        // Update the node_i
-        for(auto points_it = free_points.begin(); points_it != free_points.end(); points_it++){
-            if(std::get<0>(*points_it) == std::get<1>(min_spoke)){
-                node_i = *points_it;
-                break;
-            }
-        }
-    }
-
-    return mst_length;
+double minimal_spanning_tree_prim(std::vector<std::tuple<int, double, double>> points){
+    double mst_len = 0;
+    return mst_len;
 }
 
 

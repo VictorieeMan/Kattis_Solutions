92b6c6c85db629a2fc6fef33d16901eea41bcc30
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2022-08-25 19:18:31 +0200

    Create turbo.cpp

---
+++ C++/problems/turbo.cpp
@@ -0,0 +1,21 @@
+// Created: 2022-08-25
+// Link: https://open.kattis.com/problems/turbo
+
+//Problem type:
+//Solution: 
+
+// Kattis allows all standard libraries included in C++
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <tuple>
+
+int main() {
+    
+
+    return 0;
+}

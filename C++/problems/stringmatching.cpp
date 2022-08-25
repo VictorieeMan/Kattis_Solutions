e7c4253500b0ec43268b1179b9fc7075899d664d
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2022-08-25 12:18:39 +0200

    Rund, but input needs fixing

--- C++/problems/stringmatching.cpp
+++ C++/problems/stringmatching.cpp
@@ -19,13 +19,14 @@ std::vector<int> search_for_matches(std::string pattern, std::string text){
     std::size_t text_len = text.length();
     // std::size_t pattern_len = pattern.length();
     std::size_t found = text.find(pattern);
-    if(found != std::string::npos){
-        for(std::size_t i = 0; i < text_len; i++){
-            found = text.find(pattern, found+1);
-            if(found != std::string::npos){
-                matches_idx.push_back(found);
-            }
+    for(std::size_t i = 0; i < text_len; i++){
+        if(found != std::string::npos){
+            matches_idx.push_back(found);
+        } else {
+            break;
         }
+        
+        found = text.find(pattern, found+1);
     }
     return matches_idx;
 }

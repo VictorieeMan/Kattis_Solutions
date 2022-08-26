08358b0f4a1a754f785dd6297e1bf9e5ce7d22a2
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2022-08-26 09:06:25 +0200

    Time Limit Exceeded
    Test cases
    4/13
    
    CPU
    > 1.00 s

--- C++/problems/turbo/turbo_v2.cpp
+++ C++/problems/turbo/turbo_v2.cpp
@@ -30,7 +30,33 @@ int main() {
         array.push_back(numb);
     }
 
-    
+    std::vector<int> phase(n);
+    for(int i = 0; i < n; i++){
+        phase[i] = i+1;
+    }
+
+    int phases = n-1; // Max index of the phases array.
+    int front_idx = 0;
+    int back_idx = 0;
+
+    for(int i = 0; i < n; i++){
+        if(i % 2 == 0){
+            int key = phase[front_idx];
+            ++front_idx;
+
+            int idx = find_index(key, array);
+            array.erase(array.begin()+idx);
+            std::cout << idx << std::endl;
+        } else if(i % 2 == 1){
+            int key = phase[phases-back_idx];
+            ++back_idx;
+
+            int idx = find_index(key, array);
+            array.erase(array.begin()+idx);
+            std::cout << array.size()-idx << std::endl;
+        }
+    }
+
     
     return 0;
 }

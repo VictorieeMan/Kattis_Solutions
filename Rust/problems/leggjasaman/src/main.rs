4281f11acc13f7030cb2f62140e9644cfe8988b3
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2023-10-03 09:54:13 +0200

    Solved

--- Rust/problems/leggjasaman/src/main.rs
+++ Rust/problems/leggjasaman/src/main.rs
@@ -1,6 +1,21 @@
 //Created: 2023-10-03
 //https://open.kattis.com/problems/leggjasaman
 
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+
+    // Read the first line as an integer
+    let mut line = String::new();
+    stdin.read_line(&mut line).unwrap();
+    let n: i32 = line.trim().parse().unwrap();
+
+    // Read the second line as an integer
+    line = String::new();
+    stdin.read_line(&mut line).unwrap();
+    let m: i32 = line.trim().parse().unwrap();
+
+    // Print the sum of the two integers
+    println!("{}", n + m);
 }

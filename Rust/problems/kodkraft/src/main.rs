8a0049a487a6ec5071847e82f536af6e0afc19b5
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2023-10-03 10:11:45 +0200

    created

--- Rust/problems/kodkraft/src/main.rs
+++ Rust/problems/kodkraft/src/main.rs
@@ -1,3 +1,12 @@
+//Created: 2023-10-03
+//https://open.kattis.com/problems/kodkraft
+
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+
+    // Read the first line of integers
+    let mut line1 = String::new();
+    
 }

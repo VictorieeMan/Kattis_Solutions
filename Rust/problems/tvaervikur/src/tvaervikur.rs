672c3ce793f0bb5145728637e697f2415d3a4916
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2023-10-03 17:28:08 +0200

    Created new project

---
+++ Rust/problems/tvaervikur/src/tvaervikur.rs
@@ -0,0 +1,9 @@
+//Created: 2023-10-03 by @VictorieeMan
+//https://open.kattis.com/problems/tvaervikur
+//Repository URL: https://github.com/VictorieeMan/Kattis_Solutions
+
+use std::io;
+
+fn main() {
+	println!("Hello, world!");
+}

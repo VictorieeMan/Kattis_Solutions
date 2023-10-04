c6bc9e607a456f01d8cf29f333ecbbbfb3c9e741
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2023-10-04 16:32:50 +0200

    Added in new quick_check in old code
    and it works, no wrong answer disturbance

--- Rust/problems/tvaervikur/src/tvaervikur.rs
+++ Rust/problems/tvaervikur/src/tvaervikur.rs
@@ -61,227 +61,217 @@ fn quick_check(player_i: i32, players_sorted: &mut Vec<i32>) -> bool {
 }
 
 fn find_best_player_rank(player_id: usize, players: &Vec<i32>, players_sorted: &mut Vec<i32>) -> i32 {
-	/*
-	Function returns the best rank for player_i, as of conjecture 1, player_i
-	is guaranteed to be among the last two players. Hence either a 1 or a two is
-	returned. THe guick_check() function uses a heuristic to determine if
-	player_i won rank 1, if it returns 0, then further analysis is needed.
-	*/
-	////////////////////
-	//If only one player
-	if players.len() == 1 {
-		return 1;
-	}
+    /*
+    Function returns the best rank for player_i, as of conjecture 1, player_i
+    is guaranteed to be among the last two players. Hence either a 1 or a two is
+    returned. THe guick_check() function uses a heuristic to determine if
+    player_i won rank 1, if it returns 0, then further analysis is needed.
+    */
+    ////////////////////
+    //If only one player
+    if players.len() == 1 {
+        return 1;
+    }
 
-	/////////////////////////
-	//If more than one player
-	//Extract player_i from players
-	let player_i = players[player_id];
+    /////////////////////////
+    //If more than one player
+    //Extract player_i from players
+    let player_i = players[player_id];
 
-	/////////////////////
-	//If only two players
-	if players.len() == 2 {
-		if player_i >= players[1] {
-			return 1;
-		} else {
-			return 2;
-		}
-	}
+    /////////////////////
+    //If only two players
+    if players.len() == 2 {
+        if player_i > players[1] {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
 
-	//////////////////////////
-	//If more than two players
+    //////////////////////////
+    //If more than two players
 
-	///////////////////////
-	//If only three players
-	if players.len() == 3 {
-		//Logics for assigning c1 and c2, depending on player_id.
-		//id numbers must be in range 0..2
-		let c1_id = if player_id == 0 {1} else {0};
-		let c2_id;
-		if c1_id == 0{
-			c2_id = if player_id == 1 {2} else {1};
-		} else {// c1_id == 1;
-			c2_id = if player_id == 2 {0} else {2};
-		}
-		let mut c1;
-		let mut c2;
-		if players[c1_id] > players[c2_id]{
-			c1 = players[c1_id];
-			c2 = players[c2_id];
-		} else {
-			c1 = players[c2_id];
-			c2 = players[c1_id];
-		}
+    ///////////////////////
+    //If only three players
+    if players.len() == 3 {
+        //Extract the two challenger players, c1 and c2
+        for i in 1..players.len() {
+            if i != player_id {
+                let c1 = players[i];
+                let c2 = players[(i+1)%3];
 
-		//Let c1 and c2 figth each other, till one is left standing.
-		c1 -= c2;
-		c2 = 0;
+                //Let c1 and c2 figth each other, till one is left
+                let challenger = (c1-c2).abs();
+                if challenger < player_i {
+                    return 1;
+                } else {
+                    return 2;
+                }
+            }
+        }
+    }
 
-		let challenger = c1;
-		if player_i >= challenger {
-			return 1;
-		} else {
-			return 2;
-		}
-	}
+    ////////////////////////////
+    //If more than three players
 
-	////////////////////////////
-	//If more than three players
+    //Quick check if player_i wins rank 1
+    if quick_check(player_i, players_sorted) {
+        // No more investigation needed, player_i wins rank 1; exit.
+        return 1;
+    }
 
-	// Quick check if player_i wins rank 1
-	if quick_check(player_i, players_sorted) {
-		// No more investigation needed, player_i wins rank 1; exit.
-		return 1;
-	}
+    //Add the rest of the players to a max heap. (these are the challengers)
+    //Rust heaps are max by default.
+    let mut challenger_heap: BinaryHeap<_> = players.iter().enumerate()
+        .filter(|&(idx, _)| idx != player_id)
+        .map(|(_, &player)| player)
+        .collect();
 
-	//Add the rest of the players to a max heap. (these are the challengers)
-	//Rust heaps are max by default.
-	let mut challenger_heap: BinaryHeap<_> = players.iter().enumerate()
-		.filter(|&(idx, _)| idx != player_id)
-		.map(|(_, &player)| player)
-		.collect();
+    //find last standing player in players, to later challenge player_i
+    //Strategy designed to weaken the strong players first
 
-	//find last standing player in players, to later challenge player_i
-	//Strategy designed to weaken the strong players first
+    //Extract top 3 players, use third as a peak forward.
+    let mut top1 = challenger_heap.pop().unwrap();
+    let mut top2 = challenger_heap.pop().unwrap();
+    let mut top3 = challenger_heap.pop().unwrap();
+    let mut more_than_two: bool = true;
+    let mut first_run: bool = true;
 
-	//Extract top 3 players, use third as a peak forward.
-	let mut top1 = challenger_heap.pop().unwrap();
-	let mut top2 = challenger_heap.pop().unwrap();
-	let mut top3 = challenger_heap.pop().unwrap();
-	let mut more_than_two: bool = true;
-	let mut first_run: bool = true;
+    while first_run || challenger_heap.len() > 1{
+        // while top2 > 0 && top1 > 0{
+        //  top1 -= 1;
+        //  top2 -= 1;
+        // }
+        //Equivalent to the while loop above, but faster
+        top1 -= top2;
+        top2 = 0;
 
-	while first_run || challenger_heap.len() > 1{
-		//Simulating the two strongest fighting till one is left standing.
-		top1 -= top2;
-		top2 = 0;
+        if more_than_two && top2 < top3 {
+            //Indicates that it's time to extract a new top3
+            if top1 > 0 {
+                challenger_heap.push(top1);
+            }
+            if top2 > 0 {
+                challenger_heap.push(top2);
+            }
+            if top3 > 0 {
+                challenger_heap.push(top3);
+            }
+            more_than_two = if challenger_heap.len() > 2 {true} else {false};
 
-		if more_than_two && top2 < top3 {
-			//Indicates that it's time to extract a new top3
-			if top1 > 0 {
-				challenger_heap.push(top1);
-			}
-			if top2 > 0 {
-				challenger_heap.push(top2);
-			}
-			if top3 > 0 {
-				challenger_heap.push(top3);
-			}
-			more_than_two = if challenger_heap.len() > 2 {true} else {false};
+            if challenger_heap.len() > 1{
+                //Extract new top3
+                top1 = challenger_heap.pop().unwrap();
+                top2 = challenger_heap.pop().unwrap();
+                if more_than_two {
+                    top3 = challenger_heap.pop().unwrap();
+                }
+            }
+        }
 
-			if challenger_heap.len() > 1{
-				//Extract new top3
-				top1 = challenger_heap.pop().unwrap();
-				top2 = challenger_heap.pop().unwrap();
-				if more_than_two {
-					top3 = challenger_heap.pop().unwrap();
-				}
-			}
-		}
+        if more_than_two != true && (top1 == 0 || top2 == 0){
+            if top1 > 0 {
+                challenger_heap.push(top1);
+            }
+            if top2 > 0 {
+                challenger_heap.push(top2);
+            }
+            break;
+        }
+    }
 
-		if more_than_two != true && (top1 == 0 || top2 == 0){
-			if top1 > 0 {
-				challenger_heap.push(top1);
-			}
-			if top2 > 0 {
-				challenger_heap.push(top2);
-			}
-			break;
-		}
-	}
+    //Testing out some (to me) new rust assignment syntax
+    let last_standing = 
+    if challenger_heap.is_empty() {
+        0
+    } else {
+        challenger_heap.pop().unwrap()
+    };
 
-	//Testing out some (to me) new rust assignment syntax
-	let last_standing = 
-	if challenger_heap.is_empty() {
-		0
-	} else {
-		challenger_heap.pop().unwrap()
-	};
-
-	if(player_i >= last_standing) {
-		return 1;
-	} else {
-		return 2;
-	}
+    if(last_standing <= player_i) {
+        return 1;
+    } else {
+        return 2;
+    }
 }
 
 fn main() {
-	let stdin = io::stdin();
+    let stdin = io::stdin();
 
-	// Reading the first line
-	let mut line = String::new();
-	stdin.read_line(&mut line).unwrap();
+    // Reading the first line
+    let mut line = String::new();
+    stdin.read_line(&mut line).unwrap();
 
-	//Parsing to integers n and B
-	// n is number of contestants
-	// B is Hp damage from encounter
-	let mut iter = line.split_whitespace();
-	let n: i32 = iter.next().unwrap().parse().unwrap();
-	let b: i32 = iter.next().unwrap().parse().unwrap();
+    //Parsing to integers n and B
+    // n is number of contestants
+    // B is Hp damage from encounter
+    let mut iter = line.split_whitespace();
+    let n: i32 = iter.next().unwrap().parse().unwrap();
+    let b: i32 = iter.next().unwrap().parse().unwrap();
 
-	// Reading the second line
-	line = String::new();
-	stdin.read_line(&mut line).unwrap();
+    // Reading the second line
+    line = String::new();
+    stdin.read_line(&mut line).unwrap();
 
-	//Parsing to integer vector players
-	// players is vector of Hp for each contestant
-	let mut iter = line.split_whitespace();
-	let mut players: Vec<i32> = Vec::new();
-	for _ in 0..n {
-		//Divide all values in hp_v by B, rounded up, to get strenght of each player
-		let strength = ceil_div(iter.next().unwrap().parse().unwrap(), b);
-		players.push(strength);
-	}
+    //Parsing to integer vector players
+    // players is vector of Hp for each contestant
+    let mut iter = line.split_whitespace();
+    let mut players: Vec<i32> = Vec::new();
+    for _ in 0..n {
+        //Divide all values in hp_v by B, rounded up, to get strenght of each player
+        let strength = ceil_div(iter.next().unwrap().parse().unwrap(), b);
+        players.push(strength);
+    }
 
-	/* Problem formulation:
-	There are n contestants, each with players[i] hp. They are identified by their
-	index i in hp_v. At every moment there are still alive = n-dead contestants
-	left.Each time two collide in a fight, they lose B hp each. When a contest-
-	ant's hp reaches 0, they are eliminated and removed from the game:
-	dead++, alive-- and player i dies at rank = alive+1.
+    /* Problem formulation:
+    There are n contestants, each with players[i] hp. They are identified by their
+    index i in hp_v. At every moment there are still alive = n-dead contestants
+    left.Each time two collide in a fight, they lose B hp each. When a contest-
+    ant's hp reaches 0, they are eliminated and removed from the game:
+    dead++, alive-- and player i dies at rank = alive+1.
 
-	We want to iterate through all combinations of possible games, and find the
-	highest achievable rank of each player i. We store the highest rank of each
-	player in a vector rank_v, where index i corresponds to player i.
-	Then we print this vector.
-	*/
+    We want to iterate through all combinations of possible games, and find the
+    highest achievable rank of each player i. We store the highest rank of each
+    player in a vector rank_v, where index i corresponds to player i.
+    Then we print this vector.
+    */
 
-	/*
-	Matematical reasoning and simplification of problem:
-	Let S be an unordered set of n players, where each player i has hp_v[i] hp.
-	We have s_i = ceil(hp_v[i]/B) strength of each player i, where strength is
-	the number of fights a player can survive. We want to find the highest rank.
+    /*
+    Matematical reasoning and simplification of problem:
+    Let S be an unordered set of n players, where each player i has hp_v[i] hp.
+    We have s_i = ceil(hp_v[i]/B) strength of each player i, where strength is
+    the number of fights a player can survive. We want to find the highest rank.
 
-	*Conjecture 1: If s_i, waits for the other to fight among them selves until
-		the very end, then s_i will be guaranteed to be among the last two.
-		*s_i rank 1: if challenger is weaker.
-		*s_i rank 2: if challenger is equal or stronger, due to the rules.
-		if true => We need to find the strenght of the challenger, and compare.
-		Conjecture 2: The weakest challenger is derived by only letting the
-			strongest players fight each other until the end game.
-	
-	These are unproven, but derived from some napkin math and intuition.
-	A prototype will now be implemented, and tested on the sample input.
-	*/
+    *Conjecture 1: If s_i, waits for the other to fight among them selves until
+        the very end, then s_i will be guaranteed to be among the last two.
+        *s_i rank 1: if challenger is weaker.
+        *s_i rank 2: if challenger is equal or stronger, due to the rules.
+        if true => We need to find the strenght of the challenger, and compare.
+        Conjecture 2: The weakest challenger is derived by only letting the
+            strongest players fight each other until the end game.
+    
+    These are unproven, but derived from some napkin math and intuition.
+    A prototype will now be implemented, and tested on the sample input.
+    */
 
-	// init rank_v with -1, indicating not yet calculated
-	let mut max_rankings: Vec<i32> = vec![-1; n as usize];	//max over all games
+    // init rank_v with -1, indicating not yet calculated
+    let mut max_rankings: Vec<i32> = vec![-1; n as usize];  //max over all games
 
 	//Sort players in descending order
 	let mut players_sorted = players.to_vec();
 	players_sorted.sort();
 
-	for i in 0..n {
-		//Find best rank for player i
-		let best_rank = find_best_player_rank(i as usize, &players, &mut players_sorted);
-		//Update max_rankings
-		// if best_rank > max_rankings[i as usize] {
-			max_rankings[i as usize] = best_rank;
-		// }
-	}
+    for i in 0..n {
+        //Find best rank for player i
+        let best_rank = find_best_player_rank(i as usize, &players, &mut players_sorted);
+        //Update max_rankings
+        // if best_rank > max_rankings[i as usize] {
+            max_rankings[i as usize] = best_rank;
+        // }
+    }
 
-	//Print max_rankings
-	for i in 0..n {
-		print!("{} ", max_rankings[i as usize]);
-	}
+    //Print max_rankings
+    for i in 0..n {
+        print!("{} ", max_rankings[i as usize]);
+    }
 }

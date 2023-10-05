41f209a8f626af252620c241fb92c04387985d32
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2023-10-05 12:00:14 +0200

    created new proj,
    firs with the new version of the ks-initiator

---
+++ Rust/problems/fodelsedagsmemorisering/src/fodelsedagsmemorisering.rs
@@ -0,0 +1,9 @@
+//Created: 2023-10-05 by @VictorieeMan
+//https://open.kattis.com/problems/fodelsedagsmemorisering
+//Repository URL: https://github.com/VictorieeMan/Kattis_Solutions
+
+use std::io;
+
+fn main() {
+	println!("Hello, world!");
+}
